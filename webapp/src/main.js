1a1b0a0f0b11164f7ab5542fb47e97cb005d776c
devpartap <devpartap15@gmail.com>
2024-04-26 22:56:18 +0530

    webapp basic layout constructed

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -1,5 +1,47 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import './assets/style.css'
+
+
+// router 
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'main',
+      component: () => import("./pages/main.vue")
+    }
+  ]
+})
+
+// Vuetify
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+const app = createApp(App)
+
+// profiling
+import house_profile from '../houseprofile.json'
+console.log(house_profile)
+
+app.provide('$hp', house_profile)
+app.provide('$g', {
+  active_room:1.1
+})
+
+// app setup
+app.use(router)
+app.use(vuetify)
+app.mount('#app')
---
+++ webapp/src/pages/main.vue
@@ -0,0 +1,153 @@
+<template>
+<v-app>
+
+    <!-- ------ Main Title Bar ------ -->
+    <v-app-bar :elevation="1" :height="60">
+
+        <template v-slot:prepend>
+            <v-app-bar-nav-icon @click="drawer = !drawer">
+                <v-icon size="x-large">mdi-menu</v-icon>
+            </v-app-bar-nav-icon>
+        </template>
+
+        <v-app-bar-title >{{$hp[$global.active_room].name}}</v-app-bar-title>
+
+        <v-badge :content="5" style="margin-right: 20px;">
+            <v-icon size="x-large">mdi-cog-outline</v-icon>
+        </v-badge>
+    </v-app-bar>
+
+    <!-- ------ Main Navigation Bar ------ -->
+    <v-navigation-drawer v-model="drawer" location="left"
+        :width="360" temporary >
+        
+        <v-card style="padding-top: 15px;" flat>
+
+            <v-card-title class="font-weight-bold">
+                HOUSE LAYOUT
+            </v-card-title>
+
+            <v-card-subtitle>
+                <v-divider :thickness="3" color="black"></v-divider><br>
+            </v-card-subtitle>
+
+            <v-expansion-panels v-for="floor in Object.keys($hp.layout).reverse()" :key="floor"
+                                flat>
+
+                <v-expansion-panel :title="m_getFloorName(floor)" variant="popout" eager ripple>
+                    <v-expansion-panel-text>
+
+                        <v-list v-for="room in $hp.layout[floor]" 
+                            density="compact">
+                            <v-list-item @click="changeActiveRoom(room)">
+                                {{ room }}
+                            </v-list-item>
+                        </v-list>
+
+                    </v-expansion-panel-text>
+                </v-expansion-panel>
+
+            </v-expansion-panels>
+        </v-card>
+
+
+        <v-card style="padding-top: 15px;" flat>
+
+            <v-card-title class="font-weight-bold">
+                ACTIONS
+            </v-card-title>
+
+            <v-card-subtitle>
+                <v-divider :thickness="3" color="black"></v-divider><br>
+            </v-card-subtitle>
+
+        </v-card>
+    </v-navigation-drawer>
+
+    
+    <!-- ------ Main Page Components ------ -->
+    <v-main style="height: 100%;">
+
+    <div style="margin-left: 20px;margin-right: 20px;align-items: center;">
+        <br>
+        <v-table density="comfortable">
+
+
+            <tbody>
+                <tr v-for="device in $hp[$global.active_room].devices" :key="device.id" >
+                    <td class="text-h6 font-weight-bold" style="color: #424242; ">
+                        {{ device.name }}
+                    </td>
+
+                    <td >
+                        <v-switch
+                        style="float: right;"
+                            v-model="device.state"
+                            color="primary"
+                            hide-details
+                            inset
+                        ></v-switch>
+                    </td>
+                </tr>
+            </tbody>
+        </v-table>
+    </div>
+
+
+    </v-main>
+
+
+    </v-app>
+</template>
+
+<script setup>
+
+import {ref,reactive,inject} from 'vue'
+
+const $hp = ref(inject('$hp'))
+const $global = reactive(inject('$g'))
+
+const drawer = ref(false)
+
+let listColors = {
+    "light":"green",
+    "fan":"blue",
+    "strip":"yellow",
+    "switch":"gery"
+}
+
+
+//   --- Getting Current Roomdata
+
+function changeActiveRoom(_room_id_)
+{
+    $global.active_room = _room_id_
+}
+
+function m_getFloorName(_floor_)
+{
+    if(_floor_ == 0)
+    {
+        return "Ground Floor"
+    }
+    if(_floor_ == 1)
+    {
+        return "1st Floor"
+    }
+    if(_floor_ == 2)
+    {
+        return "2nd Floor"
+    }
+    if(_floor_ == 3)
+    {
+        return "3rd Floor"
+    }
+    else
+    {
+        return _floor_ + "th Floor"
+    }
+}
+
+
+
+</script>
